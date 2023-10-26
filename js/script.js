cf09af7b85aeff6e2c1a5f2484326250772f12d2
Nirmal Kumar <nirmalkumarsubramani@gmail.com>
2023-10-26 23:24:26 +0530

    Changes New
    Changes New

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//Current Slide
+let i = 0;
+//Total Slides
+let j = testimonials.length;
+
+let testimonialContainer = document.getElementById("testimonial-container");
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
+
+nextBtn.addEventListener("click", () => {
+  i = (j + i + 1) % j;
+  displayTestimonial();
+});
+prevBtn.addEventListener("click", () => {
+  i = (j + i - 1) % j;
+  displayTestimonial();
+});
+
+let displayTestimonial = () => {
+  testimonialContainer.innerHTML = `
+    <p>${testimonials[i].testimonial}</p>
+    <img src=${testimonials[i].image}>
+    <h3>${testimonials[i].name}</h3>
+    <h6>${testimonials[i].job}</h6>
+  `;
+};
+window.onload = displayTestimonial;
