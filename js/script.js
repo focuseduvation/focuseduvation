f362b514ea7f6d7b989c9f708819f1405775e6ae
Nirmal Kumar <nirmalkumarsubramani@gmail.com>
2024-02-13 15:24:48 +0530

    Video and other llinks popup
    Video and other llinks popup

--- js/script.js
+++ js/script.js
@@ -25,3 +25,63 @@ let displayTestimonial = () => {
   `;
 };
 window.onload = displayTestimonial;
+
+function openVideoPopup(button) {
+  var videoSource = button.getAttribute('data-video-source');
+  var videoPlayer = document.getElementById('videoPlayer');
+  var modal = document.getElementById('modal');
+  var overlay = document.getElementById('overlay');
+  var videoSourceElement = document.getElementById('videoSource');
+
+  videoSourceElement.setAttribute('src', videoSource);
+  videoPlayer.load();
+  
+  overlay.style.display = 'flex'; // Show overlay
+  modal.style.display = 'block'; // Show modal
+}
+
+function openURL(button) {
+  var url = button.getAttribute('data-url');
+  var modal = document.getElementById('modal');
+  var overlay = document.getElementById('overlay');
+  var content = document.getElementById('content');
+
+  content.innerHTML = ''; // Clear previous content
+
+  if (url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+      // If the URL is an image
+      var img = document.createElement('img');
+      img.src = url;
+      content.appendChild(img);
+  } else if (url.match(/\.(pdf)$/) != null) {
+      // If the URL is a PDF
+      var pdfViewer = document.createElement('iframe');
+      pdfViewer.src = 'https://docs.google.com/viewer?url=' + url;
+      pdfViewer.style.width = '100%';
+      pdfViewer.style.height = '100%';
+      pdfViewer.frameBorder = '0';
+      content.appendChild(pdfViewer);
+  } else {
+      // If the URL is a webpage or other types
+      var iframe = document.createElement('iframe');
+      iframe.src = url;
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.frameBorder = '0';
+      content.appendChild(iframe);
+  }
+
+  overlay.style.display = 'flex'; // Show overlay
+  modal.style.display = 'block'; // Show modal
+}
+
+
+function closeVideoPopup() {
+  var modal = document.getElementById('modal');
+  var overlay = document.getElementById('overlay');
+  var videoPlayer = document.getElementById('videoPlayer');
+
+  modal.style.display = 'none'; // Hide modal
+  overlay.style.display = 'none'; // Hide overlay
+  videoPlayer.pause(); // Pause video
+}
